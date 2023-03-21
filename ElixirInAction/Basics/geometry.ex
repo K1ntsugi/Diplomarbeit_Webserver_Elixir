e97105c38fa3c283438ed58df3b5075dab1bd8ce
K1ntsugi <max-groetsch@gmx.de>
2023-03-21 12:10:29 +0100

    basic module template

---
+++ ElixirInAction/Basics/geometry.ex
@@ -0,0 +1,11 @@
+defmodule Geometry do
+  @moduledoc """
+  A module name must follow certain rules.
+  It starts with an  uppercase letter and is usually written in CamelCase style.
+  A module name can consist of alphanumerics, underscores, and
+  the dot (.) character.
+  """
+  def rectangle_area(a, b) do
+    a * b
+  end
+end
